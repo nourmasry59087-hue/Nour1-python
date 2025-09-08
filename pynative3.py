2d403b795401605e98e513876a3f77704dd98036
nour <nourmasry59087@gmail.com>
2025-09-08 14:50:14 +0300

    exercise 13

--- pynative3.py
+++ pynative3.py
@@ -4,17 +4,24 @@
 #print(len(nournum))
 
 #exercise 11 
-start = int (input('start:'))
-end = int (input('end :'))
-nourprime=set()
-for i in range (start , end+1) :
-    isprime=True
-    if i > 0 :
-        for n in range(2 , i) :
-            if i%n ==0 :
-                isprime=False
-                break
-        if isprime == True :
-            nourprime.add(i)
-nourprime=sorted(nourprime)
-print(nourprime)
+#start = int (input('start:'))
+#end = int (input('end :'))
+#nourprime=set()
+#for i in range (start , end+1) :
+#    isprime=True
+#    if i > 0 :
+#        for n in range(2 , i) :
+#            if i%n ==0 :
+#                isprime=False
+#                break
+#        if isprime == True :
+#            nourprime.add(i)
+#nourprime=sorted(nourprime)
+#print(nourprime)
+
+
+#exercise 13
+num=int(input("enter number : "))
+for i in range(num-1,1,-1) :
+    num=num*i
+print(num)
