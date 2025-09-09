b1826098caeac750730832772ac98f37b3867b11
nour <nourmasry59087@gmail.com>
2025-09-09 16:47:01 +0300

    exercise 2

--- pynative4.py
+++ pynative4.py
@@ -1,33 +1,28 @@
-#exercise 1
+#exercise 1 
 #list1 = [3, 6, 9, 12, 15, 18, 21]
 #list2 = [4, 8, 12, 16, 20, 24, 28]
-#list3 = [] 
+#list3 =[]
 #print("Element at odd-index positions from list one")
-#for i in range(0,6+1) :
-#    if i % 2 ==0 :  
-#        del list1 [i]
+#list1=list1[1::2]
 #print(list1)
 #print("Element at even-index positions from list two")    
-#for i in range(0,6+1) :
-#    if i % 2 !=0 :
-#        del list2 [i]
+#list2=list2[0::2]
 #print(list2)
 #print("Printing Final third list")
 #list3.extend(list1)
 #list3.extend(list2)
 #print(list3)
 
-#exercise 1 better version
-list1 = [3, 6, 9, 12, 15, 18, 21]
-list2 = [4, 8, 12, 16, 20, 24, 28]
-list3 =[]
-print("Element at odd-index positions from list one")
-list1=list1[1::2]
+
+#exercise 2
+list1 = [34, 54, 67, 89, 11, 43, 94]
+print("List After removing element at index 4")
+nour = list1[4]
+del list1[4]
 print(list1)
-print("Element at even-index positions from list two")    
-list2=list2[0::2]
-print(list2)
-print("Printing Final third list")
-list3.extend(list1)
-list3.extend(list2)
-print(list3)
+print("List after Adding element at index 2")
+list1.insert(2,nour)
+print(list1)
+print("List after Adding element at last")
+list1.append(nour)
+print(list1)
