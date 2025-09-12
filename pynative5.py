7ca6e1ffae659f9674138b6a179ba7800865f228
nour <nourmasry59087@gmail.com>
2025-09-12 18:14:47 +0300

    exercise 19

--- pynative5.py
+++ pynative5.py
@@ -11,6 +11,14 @@
 
 
 #exercise 8
-numbers = [5, 2, 8, 1, 9]
-numbers.sort()
-print(numbers)
+#numbers = [5, 2, 8, 1, 9]
+#numbers.sort()
+#print(numbers)
+
+
+#exercise 19
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+list2.reverse()
+nour = zip(list1 , list2)
+print(list(nour))
