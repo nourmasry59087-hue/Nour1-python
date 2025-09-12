783b9669be0348be9ee2bf87af6e670d5d3cd687
nour <nourmasry59087@gmail.com>
2025-09-12 17:45:34 +0300

    exercise 6

--- pynative5.py
+++ pynative5.py
@@ -1,4 +1,10 @@
 #exercise 4
-list1 = [100, 200, 300, 400, 500]
-list1.reverse()
-print(list1)
+#list1 = [100, 200, 300, 400, 500]
+#list1.reverse()
+#print(list1)
+
+
+#exercise 6
+data = [8, 2, 15, 1, 9]
+print (max(data))
+print(min(data))
