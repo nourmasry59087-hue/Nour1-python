05fbdc5735e66d1317af7697116eb9df1830adef
nour <nourmasry59087@gmail.com>
2025-09-12 17:54:16 +0300

    exercise 8

--- pynative5.py
+++ pynative5.py
@@ -5,6 +5,12 @@
 
 
 #exercise 6
-data = [8, 2, 15, 1, 9]
-print (max(data))
-print(min(data))
+#data = [8, 2, 15, 1, 9]
+#print (max(data))
+#print(min(data))
+
+
+#exercise 8
+numbers = [5, 2, 8, 1, 9]
+numbers.sort()
+print(numbers)
